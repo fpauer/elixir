00a248c9878579329e718e6a89d5c67f51d30480
venfpaue <Fernando.Pauer@ahold.com>
2019-07-29 13:35:16 -0500

    Hangman website, initial forms + make_move action

--- gallows/lib/gallows_web/controllers/hangman_controller.ex
+++ gallows/lib/gallows_web/controllers/hangman_controller.ex
@@ -4,4 +4,22 @@ defmodule GallowsWeb.HangmanController do
   def new_game(conn, _params) do
     render(conn, "new_game.html")
   end
+
+  def create_game(conn, _params) do
+    game = Hangman.new_game()
+    tally = Hangman.tally(game)
+    conn
+    |> put_session(:game, game)
+    |> render("game_field.html", tally: tally)
+  end
+
+  def make_move(conn, params) do
+    guess = params["make_move"]["guess"]
+    game = get_session(conn, :game)
+    { game_state, tally } = Hangman.make_move(game, guess)
+
+    put_in(conn.params["make_move"]["guess"], "")
+    |> render("game_field.html", tally: tally)
+  end
+
 end
--- gallows/lib/gallows_web/views/hangman_view.ex
+++ gallows/lib/gallows_web/views/hangman_view.ex
@@ -1,3 +1,9 @@
 defmodule GallowsWeb.HangmanView do
   use GallowsWeb, :view
+
+  def displayWordSoFar(tally) do
+    tally.letters
+    |> Enum.join(" ")
+  end
+
 end
