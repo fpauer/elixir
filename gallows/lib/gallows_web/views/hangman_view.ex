3f7196b738575c2666055dc59a8b846516a1cbca
venfpaue <Fernando.Pauer@ahold.com>
2019-07-30 16:01:01 -0500

    game page

--- gallows/lib/gallows_web/views/hangman_view.ex
+++ gallows/lib/gallows_web/views/hangman_view.ex
@@ -1,9 +1,26 @@
 defmodule GallowsWeb.HangmanView do
   use GallowsWeb, :view
 
-  def displayWordSoFar(tally) do
+  import GallowsWeb.HangmanView.Helpers.GameStateHelper
+
+  def new_game_button(conn) do
+    button(">New Game", to: Routes.hangman_path(conn, :create_game))
+  end
+
+  def word_so_far(tally) do
     tally.letters
     |> Enum.join(" ")
   end
 
+  def game_over?(%{ game_state: game_state }) do
+    game_state in [ :won, :lost ]
+  end
+
+  def turn(left, target) when target >= left do
+    "opacity: 1.0"
+  end
+
+  def turn(left, target) do
+    "opacity: 0.1"
+  end
 end
